9435cf65cdb0d25e5bdff63103d9e4611a401371
Aneeshpissay330 <aneeshpissay330@gmail.com>
2024-01-22 08:22:45 +0530

    updated fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 function App() {
   return (
     <div className="root">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((a, index) => (
-        <div className="square" onClick={() => console.log("d")} key={index}></div>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((a) => (
+        <div className="square" onClick={() => console.log("d")} key={a}></div>
       ))}
     </div>
   );
